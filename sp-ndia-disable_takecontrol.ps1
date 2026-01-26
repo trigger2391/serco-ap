8d73666f5d69ad9283159713ca4280bfcb57ed3a
trigger2391 <the.greyster@gmail.com>
2026-01-26 13:15:54 +1100

    Add files via upload
    version 0.1 of the script

---
+++ sp-ndia-disable_takecontrol.ps1
@@ -0,0 +1,13 @@
+# ==========================================
+# Script Name: sp-ndia-disable_exclusive_control
+# Author: Matthew Bernardin
+# Date: 2026-01-23
+# Version 0.1
+# Description: Disables Take Exclusive Control setting for Headset devices to improve Amazon Connect Functionality
+# ==========================================
+
+#Variable declarations which will store Device GUID's
+
+$Default_AD = ""
+$Default_CD = ""
+
