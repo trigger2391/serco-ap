a59466f7008a5078241950bc5d0fb3d3bc19ed4d
trigger2391 <the.greyster@gmail.com>
2026-01-28 06:31:57 +1100

    Update sp-ndia-disable_takecontrol.ps1
    Initial feature complete version of the script

--- sp-ndia-disable_takecontrol.ps1
+++ sp-ndia-disable_takecontrol.ps1
@@ -6,8 +6,37 @@
 # Description: Disables Take Exclusive Control setting for Headset devices to improve Amazon Connect Functionality
 # ==========================================
 
-#Variable declarations which will store Device GUID's
+# Install AudioDeviceCmdlet powershell module 
+# Cmdlet hosted at https://github.com/frgnca/AudioDeviceCmdlets
+
+Install-Module -Name AudioDeviceCmdlets -Force
+Import-Module AudioDeviceCmdlets
+
+# Variable Declarations
 
 $Default_AD = ""
 $Default_CD = ""
+
+# Retrieve Audio Device GUID's and pipe them to variable container
+
+Get-AudioDevice -List | foreach-object $Default_AD
+Get-AudioDevice -List | foreach-object $Default_CD  
+
+# Apply regkey to Default Audio Device and Default Communications Device that was previously pulled from OS
+
+$regPath = "HKLM:\S0FTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Capture\{$Default_AD}\Properties New-ItemProperty" 
+$key = "b3f8fa53-0004-438e-9003-51a46e139bfc"
+New-ItemProperty -Path $regPath -Name $key -Value 0 -PropertyType DWORD
+
+$regPath = "HKLM:\S0FTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Playback\{$Default_AD}\Properties New-ItemProperty" 
+$key = "b3f8fa53-0004-438e-9003-51a46e139bfc"
+New-ItemProperty -Path $regPath -Name $key -Value 0 -PropertyType DWORD
+
+$regPath = "HKLM:\S0FTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Capture\{$Default_CD}\Properties New-ItemProperty" 
+$key = "b3f8fa53-0004-438e-9003-51a46e139bfc"
+New-ItemProperty -Path $regPath -Name $key -Value 0 -PropertyType DWORD
+
+$regPath = "HKLM:\S0FTWARE\Microsoft\Windows\CurrentVersion\MMDevices\Audio\Playback\{$Default_CD}\Properties New-ItemProperty" 
+$key = "b3f8fa53-0004-438e-9003-51a46e139bfc"
+New-ItemProperty -Path $regPath -Name $key -Value 0 -PropertyType DWORD
 
