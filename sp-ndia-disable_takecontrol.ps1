0dfc44557c80b7f69255cd93fc7ae31d5076f3b8
trigger2391 <the.greyster@gmail.com>
2026-01-29 17:41:43 +1100

    Update sp-ndia-disable_takecontrol.ps1

--- sp-ndia-disable_takecontrol.ps1
+++ sp-ndia-disable_takecontrol.ps1
@@ -3,7 +3,7 @@
 # Author: Matthew Bernardin
 # Date: 2026-01-23
 # Version 0.1
-# Description: Disables Take Exclusive Control setting for Headset devices and autosets Microphone permissions to allow for all apps
+# Description: Disables Take Exclusive Control setting for Headset devices and sets grants all apps microphone permissions
 # ==========================================
 
 # Install AudioDeviceCmdlet powershell module - https://github.com/frgnca/AudioDeviceCmdlets
