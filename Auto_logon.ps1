4eec9b43a78d466239eae521fe3fff407f0102cb
trigger2391 <the.greyster@gmail.com>
2026-01-30 19:55:05 +1100

    Create Auto_logon.ps1

---
+++ Auto_logon.ps1
@@ -0,0 +1,9 @@
+# ==========================================
+# Script Name: Auto_Logon
+# Author: Matthew Bernardin
+# Date: 2026-01-30
+# Version 0.1
+# Description: Schedules serco-ap_disable_takecontrol.ps1 as a on logon task in windows 11
+# ==========================================
+
+# WIP
